94fb928a253ba6bed5d72db0c32f26b1eed48ca2
AarhamH <ahaider@cayenta.com>
2025-11-16 20:59:23 -0800

    Removed unecessary feature extraction

--- src/emotional_classifier.py
+++ src/emotional_classifier.py
@@ -16,40 +16,6 @@ NUM_EPOCHS = 50
 NUM_EMOTIONS = 27
 CSV_FILE_PATH = "./EEGEmotions/training/eeg_features_extracted.csv" 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
-
-def load_data(data_dir):
-  all_files = os.listdir(data_dir)
-  eeg_data_list = []
-  labels_list = []
-  print(f"Found {len(all_files)} files. Loading data...")
-
-  for filename in tqdm(all_files):
-    if filename.endswith(".txt"):
-      try:
-        parts = filename.split('_')
-        emotion_id = int(float(parts[1].replace('.txt','')))
-        file_path = os.path.join(data_dir, filename)
-        single_reading = np.loadtxt(file_path)
-        if single_reading.ndim != 2 or single_reading.shape[0] < 10:
-          print(f"\n[WARNING] Skipping malformed or empty file: {filename}")
-          continue
-        eeg_data_list.append(single_reading)
-        labels_list.append(emotion_id - 1)
-      except Exception as e:
-        print(f"Could not process file {filename}: {e}")
-
-  print(f"Successfully loaded {len(eeg_data_list)} readings.")
-  return eeg_data_list, labels_list
-
-def extract_features(eeg_reading):
-  mean_vals = np.mean(eeg_reading, axis=0)
-  std_vals = np.std(eeg_reading, axis=0)
-
-  feature_vector = np.concatenate((mean_vals, std_vals))
-
-  return feature_vector
-
 class EEGDataset(Dataset):
   def __init__(self, features, labels):
     self.features = torch.tensor(features, dtype=torch.float32)
